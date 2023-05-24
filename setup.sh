522c1fc3facbf1e903db986fcb04b0ea21cfd800
notona <notona@gmail.com>
2023-05-25 02:55:42 +0900

    不要な設定を削除

--- setup.sh
+++ setup.sh
@@ -8,13 +8,8 @@ chsh -s /bin/zsh
 cp -r ./home/local ~/
 mkdir -vp ~/project
 #mkdir -vp ~/work
-#mkdir -vp ~/free
 mkdir -vp ~/ext
 mkdir -vp ~/tmp
-mkdir -vp ~/tmp/vim
-mkdir -vp ~/tmp/vim/swap
-
-mkdir -p ~/.vim/bundle
 
 # https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -25,17 +20,3 @@ ln -sfn ${ROOT_DIR}/home/zshrc ~/.zshrc
 ln -sfn ${ROOT_DIR}/home/tmux.conf ~/.tmux.conf
 ln -sfn ${ROOT_DIR}/home/gitconfig ~/.gitconfig
 
-# ag はマルチバイトに対応していないので
-# pt をインストールする
-# http://blog.monochromegane.com/blog/2014/01/16/the-platinum-searcher/
-case ${OSTYPE} in
-  cygwin*)
-    cp pt/pt.exe ~/local/bin/
-    ;;
-  linux*)
-    cp pt/pt_linux_x64 ~/local/bin/pt
-    ;;
-  darwin*)
-    cp pt/pt_maxosx_64 ~/local/bin/pt
-    ;;
-esac
