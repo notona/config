ea432b026de38f0eaf49935331d2c784a9ae4f3e
notona <notona@gmail.com>
2023-05-25 02:50:38 +0900

    整理

--- setup.sh
+++ setup.sh
@@ -15,37 +15,15 @@ mkdir -vp ~/tmp/vim
 mkdir -vp ~/tmp/vim/swap
 
 mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-git clone https://github.com/Shougo/vimproc.vim ~/.vim/bundle/vimproc
+
+# https://github.com/junegunn/vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ROOT_DIR=`pwd`
 ln -sfn ${ROOT_DIR}/home/vimrc ~/.vimrc
-ln -sfn ${ROOT_DIR}/home/globalrc ~/.globalrc
 ln -sfn ${ROOT_DIR}/home/zshrc ~/.zshrc
 ln -sfn ${ROOT_DIR}/home/tmux.conf ~/.tmux.conf
 ln -sfn ${ROOT_DIR}/home/gitconfig ~/.gitconfig
-
-# install vimproc
-# NeoBundleInstall の前に実行しないとエラーになる
-(
-  cd ~/.vim/bundle/vimproc
-  case ${OSTYPE} in
-    cygwin*)
-      make -f make_cygwin.mak
-      ;;
-    msys*)
-      mingw32-make -f make_mingw64.mak
-      ;;
-    linux*)
-      make
-      ;;
-    darwin*)
-      make
-      ;;
-  esac
-)
-
-vim -c NeoBundleInstall! -c ":q" -c ":q"
 
 # ag はマルチバイトに対応していないので
 # pt をインストールする
